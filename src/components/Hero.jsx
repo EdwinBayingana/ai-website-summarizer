fe3c3115565bb6ec770168f44abe4c32edc7082e
Edwin Bayingana <108181471+EdwinBayingana@users.noreply.github.com>
2023-05-19 15:09:12 +0200

    update the Github repository link in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open('https://github.com/EdwinBayingana')}
+          onClick={() => window.open('https://github.com/EdwinBayingana/summify')}
           className="black_btn"
         >
           Github
